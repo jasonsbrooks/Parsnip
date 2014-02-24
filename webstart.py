29c931538235767f279bb48a1ea8ed459e257fd6
jasonsbrooks <jason.brooks@yale.edu>
2014-02-24 15:35:16 -0500

    Minor changes to webstart file

--- webstart.py
+++ webstart.py
@@ -8,11 +8,6 @@ from flask import send_from_directory, Blueprint, send_file
 
 
 print "Starting webapp!"
-from user.views import user
-app.register_blueprint(user, url_prefix='/user')
-
-from party.views import party
-app.register_blueprint(party, url_prefix='/party')
 
 # splash
 from splash.views import splash
