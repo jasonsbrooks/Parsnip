e9e7c55e80bca791e561a2190ac1f8da22feea62
jasonsbrooks <jason.brooks@yale.edu>
2014-02-28 22:02:02 -0500

    Added js assets and background information

--- static/javascript/home.js
+++ static/javascript/home.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
-
+    $.fn.fullpage({
+            // anchors: ['titlePage', 'vidPage', 'paperPage', 'macPage'],
+            // navigation: true,
+            // navigationPosition: 'right',
+            // navigationTooltips: ['Home', 'Video', 'White Paper', 'MAC Address Lookup'],
+            // resize: true,
+            // easing: 'linear',
+            // scrollingSpeed: 400,
+            // autoScrolling: false
+    });
 });
