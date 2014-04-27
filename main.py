6eefd90171adbd80841559d655521089d6d0c3e6
earllee <earlvlee@gmail.com>
2014-04-27 11:51:01 -0400

    heatmap update

--- main.py
+++ main.py
@@ -26,15 +26,15 @@ mail = Mail(app)
 
 # app.debug = os.getenv["DEBUG"] in ('True', 'true')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['HEAPED_DATABASE_URL']
-app.config.update(dict(
-    MAIL_SERVER = 'smtp.gmail.com',
-    MAIL_PORT = 587,
-    MAIL_USE_TLS = True,
-    MAIL_USE_SSL = False,
-    MAIL_USERNAME = 'theparsnipapp@gmail.com',
-    MAIL_PASSWORD = os.environ['PARSNIP_MAIL_PW'],
-    MAIL_DEFAULT_SENDER = 'theparsnipapp@gmail.com'
-))
+# app.config.update(dict(
+#     MAIL_SERVER = 'smtp.gmail.com',
+#     MAIL_PORT = 587,
+#     MAIL_USE_TLS = True,
+#     MAIL_USE_SSL = False,
+#     MAIL_USERNAME = 'theparsnipapp@gmail.com',
+#     MAIL_PASSWORD = os.environ['PARSNIP_MAIL_PW'],
+#     MAIL_DEFAULT_SENDER = 'theparsnipapp@gmail.com'
+# ))
 
 db = SQLAlchemy(app)
 
