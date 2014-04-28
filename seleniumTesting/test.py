55ba64e8f54cd60081ceb24f8364b8a70c34b613
Diego Torres <diegoalbertotorres@gmail.com>
2014-04-28 02:04:17 -0400

    Removed Jason from Elm city market. Extended Selenium Script.

--- seleniumTesting/test.py
+++ seleniumTesting/test.py
@@ -1,58 +1,86 @@
+# This is a complete workflow test for the program. It is meant to be run locally,
+# since it is not meant to test server errors due to conenction. Remember to
+# run reset.py before testing.
+
 from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+
 import re
-passed = 0;
-failed = 0;
+import atexit
+
+x = 1
+totalTests = 9
+
+def ender():
+	if x == 9:
+		print "All tests passed!"
+	else:
+		print "Failed at test {0}".format(x)
+	
+atexit.register(ender)
 
 driver = webdriver.Chrome();
-driver.get("http://localhost:5000/");
+driver.get("http://localhost:5000/")
 
-# Test login button
-driver.find_element_by_css_selector("a[href=\"/user/login\"]").click();
+# Test 1: login button
+driver.find_element_by_css_selector("a[href=\"/user/login\"]").click()
 assert driver.title == "parsnip | Login"
+x+=1
 
-# Test login using Pranav's account
-driver.find_element_by_name("email").send_keys("pranav.maddi@yale.edu")
-driver.find_element_by_name("password").send_keys("goodbyeworld")
-driver.find_element_by_css_selector(".sign-up-button").click();
-assert driver.title == "parsnip | Dashboard"
-
-# Test logout button
-driver.find_element_by_class_name("glyphicon glyphicon-user").click();
-driver.find_element_by_css_selector("a[href=\"/user/logout\"]").click();
-assert driver.title == "parsnip | Login"
-
-# Test registration button
-driver.find_element_by_css_selector("a[href=\"/user/register\"]").click();
+# Test 2: registration button
+driver.find_element_by_css_selector("a[href=\"/user/register\"]").click()
 assert driver.title == "parsnip | Register"
+x+=1
 
-# Test registration
-email = "tester@yale.edu"
-password = "helloworld"
+# Test 3: registration on an existing company
+newEmail = "tester@yale.edu"
+newPassword = "helloworld"
 driver.find_element_by_name("firstname").send_keys("Testy")
 driver.find_element_by_name("lastname").send_keys("Tester")
-driver.find_element_by_name("email").send_keys(email)
-driver.find_element_by_name("password").send_keys("helloworld")
-driver.find_element_by_name("password2").send_keys("helloworld")
-driver.find_element_by_css_selector(".sign-up-button").click();
-assert driver.title == "parsnip | Login"
-driver.find_element_by_name("email").send_keys(email)
-driver.find_element_by_name("password").send_keys(password)
-driver.find_element_by_css_selector(".sign-up-button").click();
+driver.find_element_by_name("email").send_keys(newEmail)
+driver.find_element_by_name("password").send_keys(newPassword)
+driver.find_element_by_name("password2").send_keys(newPassword)
+driver.find_element_by_css_selector("#select2-chosen-1").click()
+driver.find_element_by_css_selector("#select2-result-label-2").click()
+driver.find_element_by_css_selector(".sign-up-button").click()
+x+=1
+
+# Test 4: login of existing account
+driver.get("http://localhost:5000/user/login")
+driver.find_element_by_name("email").send_keys("pranav.maddi@yale.edu")
+driver.find_element_by_name("password").send_keys("goodbyeworld")
+driver.find_element_by_css_selector(".sign-up-button").click()
 assert driver.title == "parsnip | Dashboard"
+x+=1
 
-# Test floormap button
-driver.find_element_by_css_selector("a[href=\"/floorplan/my-floorplans\"]").click();
-assert driver.title == "parsnip | "
+# Test 5: accepting request to join company
+driver.find_element_by_css_selector(".notifications-menu").click()
+driver.find_element_by_css_selector(".approve-store").click()
+x+=1
 
-driver.find_element_by_id("ibeacon").click();
-driver.find_element_by_id("svgroot").click();
-svgString = driver.execute_script("return svgCanvas.getSvgString();");
-print("Test 1, verify iBeacon button works in svg-edit.");
-self.assertIn("svg_1", svgString)
+# Test 6: logout button
+driver.find_element_by_css_selector("#dropdown-for-user-info").click()
+driver.find_element_by_css_selector("a[href=\"/user/logout\"]").click()
+assert driver.title == "parsnip | Login"
+x+=1
+
+# Test 7: new account
+driver.find_element_by_name("email").send_keys(newEmail)
+driver.find_element_by_name("password").send_keys(newPassword)
+driver.find_element_by_css_selector(".sign-up-button").click()
+assert driver.title == "parsnip | Dashboard"
+x+=1
+
+# Test 8: svg-editor button
+driver.find_element_by_css_selector("a[href=\"/floorplan/my-floorplans\"]").click()
+assert driver.title == "parsnip | Modify Floorplan"
+driver.find_element_by_css_selector("a[href=\"/floorplan/edit-floorplan/1\"]").click()
+driver.find_element_by_id("ibeacon").click()
+driver.find_element_by_id("svgroot").click()
+svgString = driver.execute_script("return svgCanvas.getSvgString();")
+assert "beacon=\"yes\"" in svgString
+x+=1
 	
-
-print "Summary:"
-print "{0} tests passed".format(x)
-print "{0} tests failed".format(y)
 driver.quit();
+
 
