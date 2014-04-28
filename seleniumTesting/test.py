c78b3dff9e01ca11856034419876df3d755df121
Diego Torres <diegoalbertotorres@gmail.com>
2014-04-28 01:10:40 -0400

    Extended selenium testing!

--- seleniumTesting/test.py
+++ seleniumTesting/test.py
@@ -1,23 +1,55 @@
 from selenium import webdriver
 import re
-x = 0;
-y = 0;
+passed = 0;
+failed = 0;
 
 driver = webdriver.Chrome();
-driver.get("http://heaped.ngrok.com/");
+driver.get("http://localhost:5000/");
 
+# Test login button
+driver.find_element_by_css_selector("a[href=\"/user/login\"]").click();
+assert driver.title == "parsnip | Login"
 
-# Test 1
+# Test login using Pranav's account
+driver.find_element_by_name("email").send_keys("pranav.maddi@yale.edu")
+driver.find_element_by_name("password").send_keys("goodbyeworld")
+driver.find_element_by_css_selector(".sign-up-button").click();
+assert driver.title == "parsnip | Dashboard"
+
+# Test logout button
+driver.find_element_by_class_name("glyphicon glyphicon-user").click();
+driver.find_element_by_css_selector("a[href=\"/user/logout\"]").click();
+assert driver.title == "parsnip | Login"
+
+# Test registration button
+driver.find_element_by_css_selector("a[href=\"/user/register\"]").click();
+assert driver.title == "parsnip | Register"
+
+# Test registration
+email = "tester@yale.edu"
+password = "helloworld"
+driver.find_element_by_name("firstname").send_keys("Testy")
+driver.find_element_by_name("lastname").send_keys("Tester")
+driver.find_element_by_name("email").send_keys(email)
+driver.find_element_by_name("password").send_keys("helloworld")
+driver.find_element_by_name("password2").send_keys("helloworld")
+driver.find_element_by_css_selector(".sign-up-button").click();
+assert driver.title == "parsnip | Login"
+driver.find_element_by_name("email").send_keys(email)
+driver.find_element_by_name("password").send_keys(password)
+driver.find_element_by_css_selector(".sign-up-button").click();
+assert driver.title == "parsnip | Dashboard"
+
+# Test floormap button
+driver.find_element_by_css_selector("a[href=\"/floorplan/my-floorplans\"]").click();
+assert driver.title == "parsnip | "
+
 driver.find_element_by_id("ibeacon").click();
 driver.find_element_by_id("svgroot").click();
-test1 = driver.execute_script("return svgCanvas.getSvgString();");
+svgString = driver.execute_script("return svgCanvas.getSvgString();");
 print("Test 1, verify iBeacon button works in svg-edit.");
-if "svg_1" in test1:
-	print("Test 1 passed.");
-	x = x + 1;
-else:
-	print("Test 1 failed.");
-	y = y + 1;
+self.assertIn("svg_1", svgString)
+	
 
 print "Summary:"
 print "{0} tests passed".format(x)
